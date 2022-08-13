04e40ea0845675f71455fb9a75e2e8486971a024
Juli <julia.neuburger@24speed.at>
2022-08-13 19:13:44 +0200

    Move pageLoad

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,21 @@
-import pageLoad from "./pageLoad";
+import group from "./groups";
+import toDo from "./toDo";
+import domEdit from "./domEdit";
 
-pageLoad();
+(function pageLoad(){
+
+    const defaultGroup = new group("Default");
+
+    const button = document.getElementById("create-todo");
+    button.addEventListener("click", function(){
+
+        const newTodo = new toDo(document.getElementById("title").value, document.getElementById("description").value);
+        domEdit.createToDo(newTodo);
+
+        defaultGroup.toDos.push(newTodo);
+
+        console.log(defaultGroup);
+
+    })
+
+})
